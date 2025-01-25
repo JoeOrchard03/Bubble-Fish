8cb57c9275406a1197ea7e44a2a9418b72b1a6aa
Joe Orchard <JO278870@falmouth.ac.uk>
2025-01-25 14:32:15 +0000

    bubbles spawn and delete after delay

---
+++ Prefabs/Bubble.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+@export var minExistenceTime = 2.5
+@export var maxExistenceTime = 5
+
+func _ready():
+	# Start the timer when the scene or node is ready
+	print("Trying to start timer")
+	var delay = randf_range(minExistenceTime, maxExistenceTime)
+	$DeleteTimer.start(delay)
+
+func _on_delete_timer_timeout() -> void:
+	print("Trying to delete")
+	queue_free()  # Deletes the current node
--- Prefabs/BubbleSpawner.gd
+++ Prefabs/BubbleSpawner.gd
@@ -5,24 +5,45 @@ var randomSpawnPos
 var rng = RandomNumberGenerator.new()
 
 @export var numOfBubbles = 0
+@export var bubbleImgRadius = 85
 @export var bubblePrefab = preload("res://Prefabs/Bubble.tscn")
 
-var positions:Array[Vector2] = [Vector2(0,0)]
+@export var minimumSpawnDelay = 2.5
+@export var maximumSpawnDelay = 5
+
+var positions:Array[Vector2] = []
 
 func _ready():
 	screenHeight = get_viewport().size
-	
 	for i in numOfBubbles:
 		Spawn()
+	var delay = randf_range(minimumSpawnDelay, maximumSpawnDelay)
+	$SpawnDelay.start(delay)
+	
 
-func RandomPositions:
-	for i in numOfBubbles:
-		while positions.size()
+func RandomPositions() -> Vector2:
+	while true:
+		var pos = Vector2(rng.randf_range(bubbleImgRadius, screenHeight.x - bubbleImgRadius), rng.randf_range(bubbleImgRadius, screenHeight.y - bubbleImgRadius))
+		if not tooClose(pos):
+			return pos
+	return Vector2(0,0)
+			
+func tooClose(queryPos) -> bool:
+	for i in positions:
+		if queryPos.distance_to(i) < bubbleImgRadius * 2:
+			return true
+	return false
+		
 
 func Spawn():
 	print("Running spawn")
 	var obj = bubblePrefab.instantiate()
-	randomSpawnPos = Vector2(rng.randf_range(0, screenHeight.x), rng.randf_range(0, screenHeight.y))
-	print(randomSpawnPos)
-	obj.position = randomSpawnPos
+	var pos = RandomPositions()
+	obj.position = pos
 	add_child(obj)
+	positions.append(pos)
+
+func _on_spawn_delay_timeout() -> void:
+	Spawn()
+	var delay = randf_range(minimumSpawnDelay, maximumSpawnDelay)
+	$SpawnDelay.start(delay)
