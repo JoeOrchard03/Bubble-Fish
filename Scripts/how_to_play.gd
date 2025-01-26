bd4be68cb2913cd1fa9bdd244e4412eca8e5b1a0
Joe Orchard <JO278870@falmouth.ac.uk>
2025-01-26 17:26:48 +0000

    implemented how to play button

---
+++ Scripts/how_to_play.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+func _on_exit_button_pressed() -> void:
+	get_tree().paused = false
+	hide()
+	pass # Replace with function body.
+
+
+func _on_how_to_play_button_pressed() -> void:
+	get_tree().paused = true
+	show()
+	print("How to play pressed")
